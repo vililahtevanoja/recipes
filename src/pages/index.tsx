5c893e295cc746397f03674a8533afd6a1f4a7a1
Vili Lähtevänoja <vili.lahtevanoja@gmail.com>
2023-09-13 20:29:57 +0300

    Format

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,14 +18,18 @@ export default function Home({ recipes }: Props) {
   return (
     <div>
       {recipes.map(({ id, title, metadata }) => (
-        <Link key={id} href={`/${id}`} className="no-underline content-center align-middle">
-        <div className={`flex mb-4`}>
+        <Link
+          key={id}
+          href={`/${id}`}
+          className="no-underline content-center align-middle"
+        >
+          <div className={`flex mb-4`}>
             <div className="w-1/12 text-xl content-top">
               {metadata.lang?.toLowerCase() === 'fi' ? '🇫🇮' : '🇬🇧'}
             </div>
             <div className="w-6/12 grow align-middle">{title}</div>
             <div className="w-4/12 align-middle">{metadata.tags}</div>
-        </div>
+          </div>
         </Link>
       ))}
     </div>
