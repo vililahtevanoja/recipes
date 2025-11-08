bebe44866be72aca4fa3e94b4e973beeef14bc69
Vili Lähtevänoja <3448875+vililahtevanoja@users.noreply.github.com>
2025-11-08 15:46:54 +0200

    add lockbutton to enable wakelock for keeping screen on when cooking

--- src/routes/recipes/[slug]/+page.svelte
+++ src/routes/recipes/[slug]/+page.svelte
@@ -14,8 +14,9 @@
     ],
   })
   let wakeLock: WakeLockSentinel | undefined
+  export const wakeLockAvailable = 'wakeLock' in navigator
+
   const toggleWakeLock = async () => {
-    const wakeLockAvailable = 'wakeLock' in navigator
     if (!wakeLockAvailable) {
       return
     }
@@ -27,18 +28,28 @@
     }
   }
   export let data: PageServerData
-   export let markdownHtml = marked.parse(data.content)
-   export let title = data.title
-   export let wakeLockEnabled = wakeLock !== undefined
+  export let markdownHtml = marked.parse(data.content)
+  export let title = data.title
+  export let lockButtonText = wakeLock?.released ? '🔒' : '🔓'
 </script>
 
 <svelte:head>
-  <title>{data.title}</title>
+  <title>{title}</title>
 </svelte:head>
 
 <nav>
   <a href={resolve('/')}>Home</a>
+  {#if wakeLockAvailable}
+    <button id={'lockButton'} on:click={toggleWakeLock}>{lockButtonText}</button>
+  {/if}
 </nav>
 
 <!-- Safe @html as we are rendering static content from our own Markdown files -->
-<div>{@html marked.parse(data.content)}</div>
+<div>{@html markdownHtml}</div>
+
+<style>
+  #lockButton {
+    background-color: transparent;
+    border: none;
+  }
+</style>
